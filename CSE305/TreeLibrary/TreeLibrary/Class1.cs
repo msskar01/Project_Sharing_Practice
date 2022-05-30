680b4ecba495a6e27861bf52f0e642f141bf157f
msskar01 <37842497+msskar01@users.noreply.github.com>
2022-05-30 16:17:40 -0400

    Uploading in chunks 5
    Uploading in chunks 5

---
+++ CSE305/TreeLibrary/TreeLibrary/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TreeLibrary
+{
+    struct Node<T>
+    {
+        T element;// The data in the node 
+        public Node left { get; set; } // Left child
+        Node right; // Right child 
+        Node(T value)//Giving a node a value
+        {
+            this->element = value;
+            this->left = this->right = nullptr;
+        }
+    };
+}
